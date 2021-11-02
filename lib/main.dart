e4e6a71a10a67bc4825eed8ea4adc6ca7db4c76d
Jyoti Adhikari <adkjyoti55@gmail.com>
2021-11-02 23:32:10 +0545

    Files Added

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+void main()=>runApp(MaterialApp(
+  title: "TIC TAC TOE",
+  debugShowCheckedModeBanner: false,
+  home: HomePage(),
+
+
+
+));
